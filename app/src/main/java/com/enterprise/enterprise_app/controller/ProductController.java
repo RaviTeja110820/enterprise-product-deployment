e1416b1a2c4d4d9ec20672519250f6261512080c
raviteja <bodlaraviteja11@gmail.com>
2026-07-24 18:37:09 +0530

    green

--- app/src/main/java/com/enterprise/enterprise_app/controller/ProductController.java
+++ app/src/main/java/com/enterprise/enterprise_app/controller/ProductController.java
@@ -18,7 +18,7 @@ public class ProductController {
 
     @GetMapping("/")
     public String home() {
-        return "Enterprise Product Deployment Platform is Running!-->>Blue Deployment<<--";
+        return "Enterprise Product Deployment Platform is Running!-->>Green Deployment<<--";
     }
 
     @GetMapping("/health")
